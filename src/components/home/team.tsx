71dea1cbda4a276354f45440d61933dc8adc68f3
Carlos Delbarre <cadelbarre@gmail.com>
2023-08-04 11:38:15 -0500

    Change images

--- src/components/home/team.tsx
+++ src/components/home/team.tsx
@@ -1,20 +1,22 @@
 import Container from '../share/container'
 
+// https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80
+
 export default function Team (): JSX.Element {
   return (
     <Container className='py-16 md:py-24 grid place-items-center'>
       <header className='mb-6 text-center flex flex-col justify-center items-center max-w-[42rem] mx-auto'>
-        <h2 className='text-5xl font-bold pb-4 text-primary'>Nuestros Medicos</h2>
+        <h2 className='text-4xl md:text-5xl font-bold pb-4 text-primary'>Nuestros Medicos</h2>
         <p className='text-lg mt-2'>Los mejores de su área para ofrecer siempre lo mejor</p>
       </header>
 
       <section className='mt-10 md:mt-20'>
         <ul className='grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-16'>
-          <li className='flex flex-col md:flex-row gap-6 border tranlate-y-0 border-gray-50 hover:cursor hover:shadow-lg hover:-translate-y-2 transition duration-500 ease-in-out p-0 md:p-5 rounded-lg '>
+          <li className='flex flex-col md:flex-row gap-6 border tranlate-y-0 border-gray-50 hover:cursor hover:md:shadow-lg hover:-translate-y-2 transition duration-500 ease-in-out p-0 md:p-5 rounded-lg '>
             <img
-              src='https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
-              alt=''
-              className='rounded-lg w-full md:w-52 aspect-[4/5] object-cover'
+              src='/images/default-avatar.webp'
+              alt='avatar doctor'
+              className='rounded-lg w-full md:w-52 aspect-square sm:aspect-[4/5] object-cover opacity-40'
               loading='lazy'
             />
             <div className='flex-auto'>
@@ -28,11 +30,11 @@ export default function Team (): JSX.Element {
             </div>
           </li>
 
-          <li className='flex flex-col md:flex-row gap-6 border tranlate-y-0 border-gray-50 hover:cursor hover:shadow-lg hover:-translate-y-2 transition duration-500 ease-in-out p-0 md:p-5  rounded-lg '>
+          <li className='flex flex-col md:flex-row gap-6 border tranlate-y-0 border-gray-50 hover:cursor hover:md:shadow-lg hover:-translate-y-2 transition duration-500 ease-in-out p-0 md:p-5  rounded-lg '>
             <img
-              src='https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
-              alt=''
-              className='rounded-lg w-full md:w-52 aspect-[4/5] object-cover'
+              src='/images/default-avatar.webp'
+              alt='avatar doctor'
+              className='rounded-lg w-full md:w-52 aspect-square sm:aspect-[4/5] object-cover opacity-40'
               loading='lazy'
             />
             <div className='flex-auto'>
