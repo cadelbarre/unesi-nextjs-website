895a961b1e773a65b97c2bb1a4e7c285ce9a82d5
Carlos Delbarre <cadelbarre@gmail.com>
2024-03-27 17:48:40 -0500

    Use title component

--- src/components/home/welcome.tsx
+++ src/components/home/welcome.tsx
@@ -1,17 +1,18 @@
 import Container from '../share/container'
+import Title from '../share/title'
 
 export default function Welcome (): JSX.Element {
   return (
-    <Container className='pt-24 pb-20 md:pb-28 grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-24'>
+    <Container className='pt-24 pb-20 md:pb-28 grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-12'>
       <picture className='w-full aspect-auto relative z-0 bg-gray-200 rounded-lg'>
         <img src='/doctor_jairo_fernandez.webp' alt='Doctor Jairo Fernandez' className='rounded-lg w-full aspect-auto object-cover' loading='lazy' />
       </picture>
       <div className='self-center'>
-        <h1 className='flex flex-col text-4xl md:text-5xl mb-8 gap-1 text-primary'>
-          <span className='font-thin'>Bienvenidos a </span>
-          <span className='uppercase font-bold'>unesi Ltda</span>
-        </h1>
-        <div>
+        <Title className='font-bold tracking-tight mb-8'>
+          Bienvenidos a
+          <span>UNESI Ltda</span>
+        </Title>
+        <div className='w-[50ch]'>
           <p>En nombre de todos los miembros le damos la bienvenida a nuestra página. Trabajamos todos los días del año para proveer la mejor atención y cuidado de su salud. </p>
 
           <p>Queremos brindarle la tranquilidad que usted necesita, por eso nuestro equipo de trabajadores altamente calificado, se mantiene a la vanguardia para poner a su disposición un excelente servicio en un ambiente seguro, confortable y con una atención humanizada al usuario.</p>
