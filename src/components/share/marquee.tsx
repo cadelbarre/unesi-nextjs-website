bcee08524bcf40f7920530b63e295996d3fdb127
Carlos Delbarre <cadelbarre@gmail.com>
2024-03-27 17:48:27 -0500

    Add gradient

--- src/components/share/marquee.tsx
+++ src/components/share/marquee.tsx
@@ -5,10 +5,10 @@ export default function Marquee (): JSX.Element {
   return (
     <section className='border-y border-gray-200 py-4 sm:py-8 w-full'>
       <div className='container mx-auto  [&>img]:object-contain'>
-        <MarqueeCheck pauseOnHover>
+        <MarqueeCheck pauseOnHover gradient>
           {
           certificateNames.map(name => {
-            return <img key={name} src={`/certificates/${name}.webp`} alt={`unesi-certificate-${name}`} className='mr-10 h-14 sm:h-24' loading='lazy' />
+            return <img key={name} src={`/logos-estudios/${name}.webp`} alt={`logo-estudios-unesi-${name}`} className='mr-10 h-14 sm:h-20' loading='lazy' />
           })
         }
         </MarqueeCheck>
