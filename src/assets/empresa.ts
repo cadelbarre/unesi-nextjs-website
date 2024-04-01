4bd14fb3372baebc4b798fe56a6a55baa930a10d
Carlos Delbarre <cadelbarre@gmail.com>
2024-04-01 18:00:42 -0500

    Change json to javascript object

---
+++ src/assets/empresa.ts
@@ -0,0 +1,30 @@
+const unesi = {
+  ubicacion: {
+    direccion: 'Edificio Continental Medical Center - Carrera 49C # 80 - 125, Consul 506.',
+    ciudad: 'Barranquilla, Colombia'
+  },
+  email: 'info@unesi.com.co',
+  telefonos: '(605) 3912675 - (605) 315 261-8695',
+  medicos: [
+    {
+      nombre: 'Doctor Jairo Fernandez',
+      especialidad: 'Neurocirujanoendovascular',
+      imagen: '/doctor_jairo_fernandez.webp',
+      informacion: `
+      Médico Cirujano por la Universidad Libre de Barranquilla y especialista en Neurocirugía.
+
+Amplia experiencia y trayectoria de más 20 años con más de 7.000 tratamientos endovasculares realizadados en el área de HEMODINAMIA.`
+    },
+    {
+      nombre: 'Doctor Juan Perez',
+      especialidad: 'Neurocirujanoendovascular',
+      imagen: '/images/default-avatar.webp',
+      informacion: `
+      Quia illum aut in beatae. Possimus dolores aliquid accusantium aut in ut non assumenda. 
+      
+      Enim iusto molestias aut deleniti eos aliquid magnam molestiae. At et non possimus ab. Magni labore molestiae nulla qui.`
+    }
+  ]
+}
+
+export default unesi
