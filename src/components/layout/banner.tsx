e8fcfb607d3b93ce18ce7d33b70a8f93dc5c7d77
Carlos Delbarre <cadelbarre@gmail.com>
2024-04-01 17:56:43 -0500

    Change color background

--- src/components/layout/banner.tsx
+++ src/components/layout/banner.tsx
@@ -1,14 +1,16 @@
 import { Mail, Pin, Phone } from '@/components/share/icons'
 import Container from '@/components/share/container'
-import { email, telefonos, ubicacion } from '@/assets/empresa.json'
+import unesi from '@/assets/empresa'
 
 export default function Banner (): JSX.Element {
+  const { email, telefonos, ubicacion } = unesi
+
   const cellphone = telefonos.split(' - ')[1] // get only the cellphone
   const justNumber = cellphone.replace(/[-\s()]/g, '') // remove space, - and ()
   const phoneToCall = justNumber.padStart(14, '+') // add + at the begining
 
   return (
-    <section className='bg-primary max-w-screen p-3 text-sm tracking-wide hidden md:block '>
+    <section className='bg-primary-black max-w-screen p-3 text-sm tracking-wide hidden md:block '>
       <Container>
         <ul className='text-white text-sm font-medium flex flex-wrap items-center justify-between [&>li]:flex gap-y-2 [&>li]:gap-x-2 [&>li]:flex-row [&>li]:items-center [&>li]:justify-center'>
           <li className='text-balance'>
