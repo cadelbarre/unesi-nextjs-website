ec4edd07983265e821bc4e6d49978dd006530922
Carlos Delbarre <cadelbarre@gmail.com>
2024-04-01 18:01:07 -0500

    Refactor indicators component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,35 +20,8 @@ export default function Home (): JSX.Element {
       <Doctor />
       <Marquee />
       <Team />
-      {/* <Indicators /> */}
-      <Container className='py-24 bg-primary-black rounded-xl'>
-        <dl className='grid grid-cols-2 md:grid-cols-4 gap-8 first:[&>div]:border-0 [&>div]:border-l [&>div]:border-gray-50 [&>div]:border-opacity-20 mt-8 text-white'>
-          <div className='pl-6 flex flex-col space-y-2 text-center'>
-            <dt className='text-4xl md:text-6xl font-extrabold'>
-              +<CountUp end={8000} duration={5} enableScrollSpy scrollSpyOnce />
-            </dt>
-            <dd className=''>Pacientes Atendidos</dd>
-          </div>
-          <div className='pl-6 flex flex-col space-y-2 text-center'>
-            <dt className='text-4xl  md:text-6xl font-extrabold'>
-              <CountUp end={experienceYear} duration={5} enableScrollSpy scrollSpyOnce />
-            </dt>
-            <dd className=''>Años de Experiencia</dd>
-          </div>
-          <div className='pl-6 flex flex-col space-y-2 text-center'>
-            <dt className='text-4xl  md:text-6xl font-extrabold'>
-              +<CountUp end={12500} duration={5} enableScrollSpy scrollSpyOnce />
-            </dt>
-            <dd className=''>Procedimientos Realizados</dd>
-          </div>
-          <div className='pl-6 flex flex-col space-y-2 text-center'>
-            <dt className='text-4xl  md:text-6xl font-extrabold'>
-              <CountUp end={23} duration={5} enableScrollSpy scrollSpyOnce />
-            </dt>
-            <dd className=''>Convenios con Clinicas</dd>
-          </div>
-        </dl>
-      </Container>
+      <Indicators />
+      {/* Logos de clinicas */}
       <Testimonials />
     </>
   )
