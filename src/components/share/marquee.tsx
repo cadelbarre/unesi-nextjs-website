be707a07c73f3a32d1b6139510e35ecaae612c3a
Carlos Delbarre <cadelbarre@gmail.com>
2024-04-01 17:55:47 -0500

    Remove pause when user hover

--- src/components/share/marquee.tsx
+++ src/components/share/marquee.tsx
@@ -5,7 +5,7 @@ export default function Marquee (): JSX.Element {
   return (
     <section className='border-y border-gray-200 py-4 sm:py-8 w-full'>
       <div className='container mx-auto  [&>img]:object-contain'>
-        <MarqueeCheck pauseOnHover gradient>
+        <MarqueeCheck gradient>
           {
           certificateNames.map(name => {
             return <img key={name} src={`/logos-estudios/${name}.webp`} alt={`logo-estudios-unesi-${name}`} className='mr-10 h-14 sm:h-20' loading='lazy' />
