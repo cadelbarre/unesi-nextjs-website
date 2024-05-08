da9e2e49503d49c4fcbc65521df299823d36e8ef
Carlos Delbarre <cadelbarre@gmail.com>
2024-05-08 16:13:48 -0500

    add line-heigth

--- src/app/nosotros/politicas-de-calidad/page.tsx
+++ src/app/nosotros/politicas-de-calidad/page.tsx
@@ -14,7 +14,7 @@ export default function PoliticasCalidad (): JSX.Element {
             <div className='h-60 aspect-auto mb-12 sm:mb-20 lg:mb-24 rounded-lg shadow-lg shadow-primary/20'>
               <img src='/images/politica_de_calidad.webp' alt='imagen politica de calidad' loading='lazy' className=' object-cover h-full w-full object-bottom rounded-lg' />
             </div>
-            <p className='mb-12'>Nuestro grupo de profesionales brinda atención médica especializada por medio de procedimientos de neurocirugia y neurocirugia intervencionista, encaminados hacia el bienestar y satisfacción de los pacientes que acuden a las instituciones prestadoras de salud con las cuales tenemos convenio de servicio y a los pacientes que llegan a nuestro consultorio, respaldados en la trayectoria y experiencia de nuestros médicos, quienes cuentan con excelente reconocimiento y confianza en el mundo, gracias a la certificación nacional e internacional de los estudios realizados plenamente corroborables</p>
+            <p className='mb-12 leading-[1.7]'>Nuestro grupo de profesionales brinda atención médica especializada por medio de procedimientos de neurocirugia y neurocirugia intervencionista, encaminados hacia el bienestar y satisfacción de los pacientes que acuden a las instituciones prestadoras de salud con las cuales tenemos convenio de servicio y a los pacientes que llegan a nuestro consultorio, respaldados en la trayectoria y experiencia de nuestros médicos, quienes cuentan con excelente reconocimiento y confianza en el mundo, gracias a la certificación nacional e internacional de los estudios realizados plenamente corroborables</p>
 
             <dl className='grid max-w-xl grid-cols-1 gap-x-14 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16'>
               {
@@ -24,7 +24,7 @@ export default function PoliticasCalidad (): JSX.Element {
                       <dt className='text-xl tracking-tight font-bold text-primary-black text-balance'>
                         {politica.title}
                       </dt>
-                      <dd className='mt-2 text-base text-gray-800 text-balance'>{politica.description}</dd>
+                      <dd className='mt-2 text-base leading-[1.7] text-gray-800 text-balance'>{politica.description}</dd>
                     </div>
                   )
                 })
