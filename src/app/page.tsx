ab77d7f0e6e5b0d9b00729d1ab1c7b0442e34894
Carlos Delbarre <cadelbarre@gmail.com>
2024-05-08 16:14:28 -0500

    add component on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,26 @@
 'use client'
+import Cta from '@/components/home/CTA'
+import Agreements from '@/components/home/agreements'
 import Doctor from '@/components/home/doctor'
 import Indicators from '@/components/home/indicators'
 import Slider from '@/components/home/slider'
-import Team from '@/components/home/team'
+import Videos from '@/components/home/videos'
 import Welcome from '@/components/home/welcome'
-import Container from '@/components/share/container'
 import Marquee from '@/components/share/marquee'
 import Testimonials from '@/components/share/testimonials'
 
-import CountUp from 'react-countup'
-
 export default function Home (): JSX.Element {
-  const experienceYear = new Date().getFullYear() - 2000
-
   return (
     <>
       <Slider />
       <Welcome />
       <Doctor />
       <Marquee />
-      <Team />
+      <Videos />
       <Indicators />
-      {/* Logos de clinicas */}
+      <Cta />
       <Testimonials />
+      <Agreements />
     </>
   )
 }
