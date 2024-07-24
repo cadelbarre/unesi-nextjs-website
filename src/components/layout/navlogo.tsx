9c0dc9b06214761b04a88b805bedba36581525fd
Carlos Delbarre <cadelbarre@gmail.com>
2024-07-24 17:34:15 -0500

    add logo unesi

--- src/components/layout/navlogo.tsx
+++ src/components/layout/navlogo.tsx
@@ -3,7 +3,7 @@ export default function NavLogo (): JSX.Element {
   return (
     <a href='/' className='flex items-center cursor-pointer'>
       <img
-        src='https://flywebwp.websitelayout.net/wp-content/uploads/2024/02/logo.png'
+        src='/logos/unesi-logo-color.png'
         className='h-10 md:h-14'
         alt='unesi logo color'
       />
