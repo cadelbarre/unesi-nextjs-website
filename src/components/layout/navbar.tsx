afa79df4ab1ce9bf709395e32102d41bc6b12a6b
Carlos Delbarre <cadelbarre@gmail.com>
2024-07-24 17:34:36 -0500

    add transition

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -11,7 +11,9 @@ export default function Header (): JSX.Element {
         <Container className='flex flex-wrap items-center justify-between mx-auto p-4'>
           <NavLogo />
           <div className='flex md:order-2'>
-            <a href='https://api.whatsapp.com/send?phone=573152618695&amp;text=Hola quisiera mayor información...' target='_blank' className='text-white bg-primary hover:bg-primary-black focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-3 py-3 sm:px-6 sm:py-3 text-center mr-3 md:mr-0' rel='noreferrer'>Solicitar Cita</a>
+            <a href='https://api.whatsapp.com/send?phone=573152618695&amp;text=Hola quisiera mayor información...' target='_blank' className='text-white bg-primary hover:bg-primary-black focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-3 py-3 sm:px-6 sm:py-3 text-center mr-3 md:mr-0 transition-colors duration-200 ease-in' rel='noreferrer'>
+              Solicitar Cita
+            </a>
             <button data-collapse-toggle='navbar-sticky' type='button' className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600' aria-controls='navbar-sticky' aria-expanded='false'>
               <span className='sr-only'>Abrir Menú</span>
               <svg className='w-5 h-5' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 17 14'>
