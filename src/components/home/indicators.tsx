ecd7c95501df49d8598e060c3fa600a3196ea965
Carlos Delbarre <cadelbarre@gmail.com>
2024-07-24 23:01:43 -0500

    fix padding

--- src/components/home/indicators.tsx
+++ src/components/home/indicators.tsx
@@ -6,7 +6,7 @@ export default function Indicators (): JSX.Element {
   const experienceYear = new Date().getFullYear() - 2000
 
   return (
-    <section className='w-full relative pt-12 lg:pt-24 px-6'>
+    <section className='w-full relative pt-12 lg:pt-18 px-6'>
       <Container className='py-16 bg-primary-black rounded-xl bg-[url(/images/overlay-indicators.webp)]'>
         <dl className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 first:[&>div]:border-0 md:[&>div]:border-l [&>div]:border-gray-100/40 mt-2 text-white'>
           <div className='sm:pl-6 flex flex-col space-y-2 text-center'>
