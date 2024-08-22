da8725ec614b5e55d2f1a76f2b8d56aa9111c3a9
Carlos Delbarre <cadelbarre@gmail.com>
2024-08-22 11:42:30 -0500

    change instagram url

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -34,7 +34,7 @@ export default function Contacto (): JSX.Element {
             </dl>
 
             <div className='text-gray-700 flex rounde-full'>
-              <a href='https://www.instagram.com/unesiltda/' target='_blank' className='transition duration-500 ease-in-out scale-100 hover:scale-110 p-1' rel='noreferrer'>
+              <a href='https://www.instagram.com/unesi.sas/' target='_blank' className='transition duration-500 ease-in-out scale-100 hover:scale-110 p-1' rel='noreferrer'>
                 <svg width='24px' height='24px' strokeWidth='1.5' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' color='currentColor'><path d='M12 16a4 4 0 100-8 4 4 0 000 8z' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' /><path d='M3 16V8a5 5 0 015-5h8a5 5 0 015 5v8a5 5 0 01-5 5H8a5 5 0 01-5-5z' stroke='currentColor' strokeWidth='1.5' /><path d='M17.5 6.51l.01-.011' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' /></svg>
               </a>
             </div>
