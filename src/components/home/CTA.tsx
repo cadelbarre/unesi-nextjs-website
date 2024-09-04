89492a49e49d9e5f59026a3c3349ab1057db26e0
Carlos Delbarre <cadelbarre@gmail.com>
2024-09-04 10:10:48 -0500

    fix padding and balance text

--- src/components/home/CTA.tsx
+++ src/components/home/CTA.tsx
@@ -1,18 +1,18 @@
 import Link from 'next/link'
 import Button from '../share/button'
 import Container from '../share/container'
-//  bg-[url(/images/overlay-indicators.webp)]
+
 export default function CTA (): JSX.Element {
   return (
     <section className='relative py-20 mt-24 w-full bg-[url(/images/dudas-e-inquietudes.webp)] bg-center bg-no-repeat bg-cover'>
       <div className='bg-primary-black/80 absolute z-10 inset-0 mix-blend-multiply' />
-      <Container className='relative z-20 mx-auto px-6 gap-x-12 justify-between md:flex md:flex-col xl:px-8'>
+      <Container className='relative z-20 mx-auto gap-x-12 justify-between md:flex md:flex-col px-6 xl:px-0'>
 
         <div className='max-w-xl'>
           <h3 className='text-white text-3xl font-bold sm:text-5xl'>
             ¿ Tienes dudas e inquietud ?
           </h3>
-          <p className='mt-3 text-gray-100'>
+          <p className='mt-3 text-gray-100 text-balance'>
             Conoce las principales inquietudes de nuestros pacientes, si ninguna resuelve tus dudas no dudes en contactarnos.
           </p>
         </div>
